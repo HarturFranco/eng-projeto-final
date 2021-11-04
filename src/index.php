e4d6339770fad33ed566e880c03e51a4cf733d84
HarturFranco <franco.s.arthur00@gmail.com>
2021-11-04 01:41:22 -0300

    refactor: Deletando arquivo index duplicado

--- src/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "<pre><h2>Criando um ambiente de desenvolvimento PHP com Docker Compose.</h2></pre><br/>";
-mysqli_connect("db", "root", "123");
-echo "<pre><h3>Connected to MySQL<h3></pre><br/>";
