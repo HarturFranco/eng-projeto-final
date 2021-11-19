b3b4f79ddcd8808254037e9a3c0f4c1de9abce14
felipecarvalhogodoi98 <felipecarvalhogodoi98@gmail.com>
2021-11-19 18:22:31 -0300

    feat: funcionario gerente admin

--- db/schema.sql
+++ db/schema.sql
@@ -104,3 +104,6 @@ DEFAULT CHARACTER SET = utf8;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+INSERT INTO `Funcionario`(`funNome`, `funEmail`, `funUsername`, `funSenha`, `funIsGerente`) 
+VALUES ('admin','admin@gmail.com','admin','admin','1');
