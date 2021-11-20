ea36a62b4fd9eb888fb538f68533cca3795cbda5
felipecarvalhogodoi98 <felipecarvalhogodoi98@gmail.com>
2021-11-20 10:44:19 -0300

    feat: telas de categoria

--- src/Visao/cadastro/categoria.php
+++ src/Visao/cadastro/categoria.php
@@ -1,1 +1,25 @@
-cad cate
+<?php
+// include_once "Controle/FuncionarioControle.php";
+// $funcionarioControle = new FuncionarioControle();
+?>
+
+<div class="cadastro">
+  <h1>Cadastrar Categoria</h1>
+
+  <div>
+    <form action="Controle/Controle" method="POST">
+      <div>
+        <label for="pNome">Nome:</label>
+        <input name="pNome" type="text" maxlength="50" required placeholder="Entre com o nome da categoria">
+
+        <label for="pDescricao">Username:</label>
+        <textarea maxlength="500" name="pDescricao" type="text" placeholder="Descrição da categoria" cols="0" rows="6"></textarea>
+
+      </div>
+      <div></div>
+      <input type="text" name="pCodigo" value="<?php echo '' ?>" hidden>
+      <input type="text" name="classeAcao" value="CategoriaControle/cadastrar" hidden>
+      <button class="primary">Cadastrar</button>
+    </form>
+  </div>
+</div>
